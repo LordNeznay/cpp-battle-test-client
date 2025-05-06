3967d7fecd8971d27d671026994ef83dddd6ed2d
a.kurulev <ankurulev@mail.ru>
2025-05-06 14:25:52 +0300

    Предкомпилированные заголовки

---
+++ src/Global.hpp
@@ -0,0 +1,12 @@
+
+#include <stdexcept>
+#include <memory>
+#include <optional>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+using UnitId = uint64_t;
